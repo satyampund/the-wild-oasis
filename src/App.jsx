f02f2ab814a68493ea294d984b2d7c70f801d1be
Satyam Pund <pundsatyam@gmail.com>
2024-03-30 15:59:05 +0530

    use styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,43 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+font-size: 30px;
+font-weight: 600;
+background-color: yellow;
+`
+
+const Button = styled.button`
+font-size: 1.4rem;
+padding: 1.2rem 1.6rem;
+font-weight: 500;
+border: none;
+border-radius: 7px;
+background-color: purple;
+color: white;
+margin: 20px;
+cursor: pointer;
+`
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0.8rem 1.2rem;
+`
+
+const StyledApp = styled.div`
+  background-color: orange;
+  padding: 20px;
+
+`
+
 const App = () => {
   return (
-    <div>
-      <h1>Vite Setup</h1>
-    </div>
+    <StyledApp>
+      <H1>Vite Setup</H1>
+      <Button>Check In</Button>
+      <Button>Check Out</Button>
+      <Input type="number" placeholder="Number of Guests"></Input>
+    </StyledApp>
   );
 };
 

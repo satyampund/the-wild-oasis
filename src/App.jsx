c9fb1c7929f57d4a10b9ad738b9a3dea10a07e61
Satyam Pund <pundsatyam@gmail.com>
2024-03-30 17:52:57 +0530

    building more resulable components

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 
 const StyledApp = styled.div`
-  background-color: orange;
+  /* background-color: orange; */
   padding: 20px;
 `
 
@@ -15,13 +16,25 @@ const App = () => {
     <>
     <GlobalStyles/>
     <StyledApp>
-      <Heading as='h1'>Vite Setup</Heading>
-      <Heading as='h2'>Check In and Out</Heading>
-      <Button>Check In</Button>
-      <Button>Check Out</Button>
-      <Heading as='h3'>Form</Heading>
-      <Input type="number" placeholder="Number of Guests"></Input>
+    <Row>
+      <Row type='horizontal'>
+           <Heading as='h1'>The Wild Oasis</Heading>
+           <div>
+            <Heading as='h2'>Check In and Out</Heading>
+            <Button>Check In</Button>
+            <Button variation='secondary' size='small'>Check Out</Button>
+           </div>
+      </Row>
+      <Row type='vertical'>
+          <Heading as='h3'>Form</Heading>
+          <form>
+            <Input type="number" placeholder="Number of Guests"></Input>
+            <Input type="number" placeholder="Number of Guests"></Input>
+          </form>
+      </Row>
+    </Row>
     </StyledApp>
+    
     </>
   );
 };

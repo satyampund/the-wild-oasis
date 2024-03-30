0d89c5a60ed30e08eaca4b0b8c1709298cffe009
Satyam Pund <pundsatyam@gmail.com>
2024-03-30 16:25:54 +0530

    Global styles with styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
 
 const H1 = styled.h1`
 font-size: 30px;
@@ -6,23 +9,7 @@ font-weight: 600;
 background-color: yellow;
 `
 
-const Button = styled.button`
-font-size: 1.4rem;
-padding: 1.2rem 1.6rem;
-font-weight: 500;
-border: none;
-border-radius: 7px;
-background-color: purple;
-color: white;
-margin: 20px;
-cursor: pointer;
-`
 
-const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0.8rem 1.2rem;
-`
 
 const StyledApp = styled.div`
   background-color: orange;
@@ -32,12 +19,15 @@ const StyledApp = styled.div`
 
 const App = () => {
   return (
+    <>
+    <GlobalStyles/>
     <StyledApp>
       <H1>Vite Setup</H1>
       <Button>Check In</Button>
       <Button>Check Out</Button>
       <Input type="number" placeholder="Number of Guests"></Input>
     </StyledApp>
+    </>
   );
 };
 

6b2b6be2da8d72449aa7aa54c296dc07bf1abaf3
Satyam Pund <pundsatyam@gmail.com>
2024-04-01 17:44:28 +0530

    fecthing cabin data

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { formatCurrency }  from './../../utils/helpers'
 
 const TableRow = styled.div`
   display: grid;
@@ -38,3 +39,35 @@ const Discount = styled.div`
   font-weight: 500;
   color: var(--color-green-700);
 `;
+
+const CabinRow = ({cabin}) => {
+
+  const {
+    id: cabinId,
+    name,
+    maxCapacity,
+    regularPrice,
+    discount,
+    image,
+    description,
+  } = cabin;
+
+  return (
+    <>
+    <TableRow role="row">
+        <Img src={image} />
+        <Cabin>{name}</Cabin>
+        <div>Fits up to {maxCapacity} guests</div>
+        <Price>{formatCurrency(regularPrice)}</Price>
+        {discount ? (
+          <Discount>{formatCurrency(discount)}</Discount>
+        ) : (
+          <span>&mdash;</span>
+        )}
+       <button>Delete</button>
+      </TableRow>
+    </>
+  );
+};
+
+export default CabinRow;
